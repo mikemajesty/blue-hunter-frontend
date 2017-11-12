5e07c2ca10b920a7b74279581b08112e7eec095c
mikemajesty <mikemajesty2011@gmail.com>
2017-11-12 20:58:34 -0200

    Add  get by title endpoint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FooterComponent } from './footer/footer.component';
 import { ContentComponent } from './content/content.component';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,9 @@ import { AppComponent } from './app.component';
     ContentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.less']
 })
+
 export class ContentComponent implements OnInit {
 
-  constructor() { }
+  booksByTitle: Array<any>;
 
-  ngOnInit() {
+  constructor(private http: HttpClient) {
+  }
+ 
+  ngOnInit(): void {
   }
 
+  getBooksByTitle(item) {
+    this.http.get(`https://blue-hunter-backend-api.herokuapp.com/book/by-title/${item}`).subscribe(data => {
+      this.booksByTitle = <Array<any>>data;
+    });
+  }
 }
