a14a7bb4a8c51b73207708c006dbf65f61a1ee7c
mikemajesty <mikemajesty2011@gmail.com>
2017-11-14 02:56:56 -0200

    Add body parser

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
-const app = express();
 const path = require('path');
-// ...
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
+const bodyParser = require('body-parser');
 
-app.use(express.static(__dirname + '/dist'));
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
 
 app.listen(process.env.PORT || 3000);
